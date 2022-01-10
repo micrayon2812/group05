71a3b9fb9b856827b4e308115777078ccf663db1
LeThiKimMy <61327839+micrayon2812@users.noreply.github.com>
2022-01-11 04:15:26 +0700

    Entertainment fix

--- src/src/Categories/Entertainment.js
+++ src/src/Categories/Entertainment.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {database} from "../Database/Firebase";
 import { useState } from 'react';
+import './View.css'
 function Entertainment() {
     const [info , setInfo] = useState([]);
 
@@ -18,37 +19,47 @@ function Entertainment() {
         })
     }
     return (
-        <div>
-        <center>
-        <h2>Book Details</h2>
-        </center>
+    //     <div>
+    //     <center>
+    //     <h2>Book Details</h2>
+    //     </center>
       
-    {
+    // {
+    //     info.map((data) => (
+    //     <Frame Name={data.Name} 
+    //            Author={data.Author} 
+    //            Buy={data.Buy}/>
+    //     ))
+    // }
+    // </div>
+
+<div class="container" >
+  <div class="row row-cols-auto">
+
+{
         info.map((data) => (
         <Frame Name={data.Name} 
                Author={data.Author} 
-               Buy={data.Buy}/>
+               Des={data.Des}
+               Ebook={data.Ebook}
+               Buy={data.Buy}
+               Cover={data.Cover}
+               />
         ))
     }
-    </div>
+ </div>
+ </div>
     )
 }
-const Frame = ({Name , Author , Buy}) => {
-    console.log(Name + " " + Author + " " + Buy);
+const Frame = ({Name , Author ,Des,Ebook, Buy,Cover}) => {
+    console.log(Name + " " + Author + " " +  Des + " " +" " + Ebook+" " + Buy+" " +Cover);
     return (
-        <center>
-            <div className="div">
-                  
-<p>NAME : {Name}</p>
-   
-                  
-<p>Author : {Author}</p>
-  
-                  
-<p>Buy : {Buy}</p>
-   
-            </div>
-        </center>
+<div className='col'>
+<img src={Cover} alt="Avatar" class="image"  />
+<div class="middle">
+  <div class="text">John Doe</div>
+</div>
+</div>
     );
 }
 export default Entertainment
