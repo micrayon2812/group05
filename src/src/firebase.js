ee6be15c4c8dde09611eb0cd01a8ce87ec8c6338
LeThiKimMy <61327839+micrayon2812@users.noreply.github.com>
2022-01-12 00:24:15 +0700

    Update APIKey

--- src/src/firebase.js
+++ src/src/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase/compat/app"
 import "firebase/compat/auth"
 
 const app = firebase.initializeApp({
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyAJZdIVDHbtCEvXdPtJYchbIXo0o5PNGsw",
+    authDomain: "rebook-d644c.firebaseapp.com",
+    projectId: "rebook-d644c",
+    storageBucket: "rebook-d644c.appspot.com",
+    messagingSenderId: "762929478324",
+    appId: "1:762929478324:web:52ce662ca1546545aa35e1",
+    measurementId: "G-WXJ69JTNYE"
 })
 
 export const auth = app.auth()
