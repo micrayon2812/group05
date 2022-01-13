b63a55b20dafe4d41ad0b757f93e82d602a49b29
dddinh19 <64194776+dddinh19@users.noreply.github.com>
2022-01-13 23:04:18 +0700

    changes

--- src/src/Database/Firebase.js
+++ src/src/Database/Firebase.js
@@ -1,25 +1,26 @@
 // import firebase from "../../../../tree/node_modules/firebase";
 // import storage from "firebase/storage";
 // import firestore from "firebase/firebase-firestore"
-import firebase from "firebase/compat";
+import firebase from "firebase/compat"
 import "firebase/compat/firestore";
 import "firebase/compat/storage"
+
 const firebaseConfig = {
-    apiKey: "AIzaSyCPlDAs8WPCPoZmpBjFIbjPhgis87C279Y",
-    authDomain: "rebook-67249.firebaseapp.com",
-    projectId: "rebook-67249",
-    storageBucket: "rebook-67249.appspot.com",
-    messagingSenderId: "1027559659854",
-    appId: "1:1027559659854:web:b0e6f37c3e8d78d10be61f"
-  };
-  if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
- }else {
-    firebase.app(); // if already initialized, use that one
- }
+  apiKey: "AIzaSyCPlDAs8WPCPoZmpBjFIbjPhgis87C279Y",
+  authDomain: "rebook-67249.firebaseapp.com",
+  projectId: "rebook-67249",
+  storageBucket: "rebook-67249.appspot.com",
+  messagingSenderId: "1027559659854",
+  appId: "1:1027559659854:web:b0e6f37c3e8d78d10be61f"
+};
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app(); // if already initialized, use that one
+}
 // export const storages = firebaseConfig.firestore;
 // export const database= firebase.firestore();
-export const storage =firebase.storage()
+export const storage = firebase.storage()
 export const database = firebase.firestore()
 
 export default firebase;
