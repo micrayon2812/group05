253433aaf6698038f9ab15cd1683a9ab9862887d
dddinh19 <64194776+dddinh19@users.noreply.github.com>
2022-01-13 20:49:44 +0700

    Categories2

--- src/src/categories/ArtsMusic.js
+++ src/src/categories/ArtsMusic.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import useFirestore from "../hooks/useFirestore";
 
-function ArtsMusic() {
+const ArtsMusic = () => {
+    const { docs } = useFirestore('Art&Music');
+    console.log(docs);
+
     return (
-        <p>lala</p>
+        <div className="img-grid">
+            {docs && docs.map(doc => (
+                <div className="img-wrap" key={doc.id}>
+                    <img src={doc.url} alt="uploaded pic" />
+                </div>
+            ))}
+        </div>
     )
 }
 
-export default ArtsMusic
+export default ArtsMusic;
