7bd15d9651d1950bcaec1a6b88b1a9466ab00ebe
ntngan <ntngan19@apcs.fitus.edu.vn>
2022-01-15 00:08:50 +0700

    tree store UI

---
+++ src/src/TreeList.js
@@ -0,0 +1,34 @@
+import tree from "./img/tree.gif";
+
+export const TreeList = [
+    {
+        name: "Tree",
+        image: tree,
+        price: 15
+    },
+    {
+        name: "Tree",
+        image: tree,
+        price: 15
+    },
+    {
+        name: "Tree",
+        image: tree,
+        price: 15
+    },
+    {
+        name: "Tree",
+        image: tree,
+        price: 15
+    },
+    {
+        name: "Tree",
+        image: tree,
+        price: 15
+    },
+    {
+        name: "Tree",
+        image: tree,
+        price: 15
+    }
+]
--- src/src/TreeStore.js
+++ src/src/TreeStore.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from "react";
+import {TreeList} from "./TreeList";
+import Tree from "./Tree";
+import "./TreeStore.css"
+
 
 function TreeStore() {
     return (
-        <p></p>
+        <div className="treeStore">
+            <h1 className="treeStoreName">Tree Store</h1>
+            <div className="treeList">
+                {TreeList.map((tree, key) => {
+                return (
+                    <Tree
+                    key={key}
+                    image={tree.image}
+                    name={tree.name}
+                    price={tree.price}
+                    />
+                );
+                })}
+            </div>
+        </div>
     )
 }
 
